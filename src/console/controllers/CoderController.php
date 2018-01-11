c6ebd35a42d89dc6879e025f8f31aae2d2ca01ec
Vitaliy <theyamshikov@yandex.ru>
2018-01-11 20:56:24 +0600

    make console help

--- src/console/controllers/CoderController.php
+++ src/console/controllers/CoderController.php
@@ -7,9 +7,15 @@ use yii2lab\console\helpers\input\Enter;
 use yii2lab\console\yii\console\Controller;
 use yii2lab\console\helpers\Output;
 
+/**
+ * Encryption and decryption of data
+ */
 class CoderController extends Controller
 {
 	
+	/**
+	 * encryption of data
+	 */
 	public function actionEncode()
 	{
 		$text = Enter::display('Enter text');
@@ -17,6 +23,9 @@ class CoderController extends Controller
 		Output::block($encrypted, 'Encrypted');
 	}
 	
+	/**
+	 * decryption of data
+	 */
 	public function actionDecode()
 	{
 		$encrypted = Enter::display('Enter encrypted');
