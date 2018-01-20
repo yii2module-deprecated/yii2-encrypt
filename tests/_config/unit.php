d87dc88cdeb3fa5c804341d26a9882583532d632
Vitaliy <theyamshikov@yandex.ru>
2018-01-20 17:44:49 +0600

    use new variant

--- tests/_config/unit.php
+++ tests/_config/unit.php
@@ -3,6 +3,9 @@
 namespace yii2lab\app;
 
 use yii2lab\app\domain\helpers\Config;
+use yii2lab\app\domain\helpers\Env;
 use yii2lab\helpers\yii\ArrayHelper;
 
-return ArrayHelper::merge(Config::get(), @include('main.php'));
+$definition = Env::get('config');
+$config = @include('main.php');
+return ArrayHelper::merge(Config::load($definition), $config);
