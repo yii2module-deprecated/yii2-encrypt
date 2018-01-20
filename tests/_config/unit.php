f62ba9cfe703603b0847232453ff27175fc2db86
Vitaliy <theyamshikov@yandex.ru>
2018-01-20 21:57:15 +0600

    fix

--- tests/_config/unit.php
+++ tests/_config/unit.php
@@ -6,6 +6,20 @@ use yii2lab\app\domain\helpers\Config;
 use yii2lab\app\domain\helpers\Env;
 use yii2lab\helpers\yii\ArrayHelper;
 
+Env::init([
+	'commands' => [],
+	'filters' => [
+		[
+			'class' => 'yii2lab\app\domain\filters\env\LoadConfig',
+			'paths' => [
+				'vendor/yii2lab/yii2-app/tests/store/app/common/config',
+				'vendor/yii2lab/yii2-app/src/domain/config',
+			],
+		],
+		'yii2lab\app\domain\filters\env\YiiEnv',
+		'yii2lab\app\domain\filters\env\NormalizeDbConfig',
+	],
+]);
 $definition = Env::get('config');
 $config = @include('main.php');
 return ArrayHelper::merge(Config::load($definition), $config);
