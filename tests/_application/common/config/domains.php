b868affce07983a07c1e92b2941d5f03ce71534b
Vitaliy <theyamshikov@yandex.ru>
2018-03-29 12:22:29 +0600

    move test config

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -4,23 +4,7 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-	'encrypt' => [
-		'class' => 'yii2module\encrypt\domain\Domain',
-		'services' => [
-			'coder' => [
-				'profiles' => [
-					'default' => [
-						'password' => 'zx1uDi4TG',
-						'iv' => 'qrk4QVgE78ukb1PH',
-					],
-					'test' => [
-						'password' => 'zxGqh96wC',
-						'iv' => 'diUeXzEr426k8dNJ',
-					],
-				],
-			],
-		],
-	],
+	'encrypt' => 'yii2module\encrypt\domain\Domain',
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
--- tests/_application/common/config/env-local.php
+++ tests/_application/common/config/env-local.php
@@ -4,7 +4,18 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-
+	'encrypt' => [
+		'profiles' => [
+			'default' => [
+				'password' => 'zx1uDi4TG',
+				'iv' => 'qrk4QVgE78ukb1PH',
+			],
+			'test' => [
+				'password' => 'zxGqh96wC',
+				'iv' => 'diUeXzEr426k8dNJ',
+			],
+		],
+	],
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/env-local.php');
